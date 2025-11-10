5aa291e4fd8ad9e4ccdfedf973171b5f77677f49
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-10 13:06:38 +0530

    Add Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <div >
-      
+    <div>
+      <NavBar />
     </div>
   );
 }
