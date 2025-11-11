da5952fa2155c5f3625af33d8461f62fefdc9306
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-11 16:51:14 +0530

    Optimizing the SignUp with the help of AuthProvider an AuthService

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,33 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
+import { SignUpProcess } from "../../service/AuthService";
+import { useAuth } from "./AuthProvider";
+import { useNavigate } from "react-router";
+export const SignUp = () => {
 
-export const SignUp: React.FC = () => {
+  interface SignUp{
+    username: string;
+    password: string;
+    fullname: string;
+    role: string;
+  }  
+ 
+
+  const navigate = useNavigate()
+  const { login } = useAuth();
+
+  //state handle
+  const [user,setUser]  = useState<SignUp>({
+    username: "",
+    password: "",
+    fullname: "",
+    role: "",
+  })  
+
+
+  //form data handle
+  const handleOnChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const {name, value} = e.target;
+    setUser((prev)=> ({...prev, [name]:value}))
+
+  }
+
+
+ // send sign updata to the server
+  const handleOnSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+     e.preventDefault()
+     const token = await SignUpProcess(user)
+     console.log(token)
+     login(token)
+     navigate("/documents")
+  }  
+
   return (
-    <Container className="mt-5 d-flex justify-content-center">
-      <Form style={{ width: "400px" }}>
-        <h2 className="text-center mb-4">Register</h2>
+    <>
+    <h1 style={{textAlign:"center"}}>Register</h1>
+    <Form className="d-flex flex-column align-items-center mt-5" onSubmit={handleOnSubmit}>
         <Form.Group className="mb-3" controlId="formGroupUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter Username" />
+          <Form.Label>username</Form.Label>
+          <Form.Control 
+           type="text"
+           placeholder="Enter Username" 
+           value={user.username}
+           name="username"
+           onChange={handleOnChange}
+           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="text" placeholder="Enter Password" />
+          <Form.Control 
+           type="password"
+           placeholder="Enter Password" 
+           value={user.password}
+           name="password"
+           onChange={handleOnChange}
+           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupFullname">
-          <Form.Label>Fullname</Form.Label>
-          <Form.Control type="email" placeholder="Enter Fullname" />
+          <Form.Label>fullname</Form.Label>
+          <Form.Control
+           type="text"
+           placeholder="Enter Fullname" 
+           value={user.fullname}
+           name="fullname"
+           onChange={handleOnChange}
+           
+        />
         </Form.Group>
+        
         <Form.Group className="mb-3" controlId="formGroupRole">
           <Form.Label>Role</Form.Label>
-          <Form.Control type="text" placeholder="Enter role" />
+          <Form.Control 
+           type="text"
+           placeholder="Enter Role" 
+           value={user.role}
+           name="role"
+           onChange={handleOnChange}
+           />
         </Form.Group>
-        <Button variant="success" type="submit" className="w-100">
-          Sign Up
-        </Button>
+        <Button variant="success" type="submit">Register</Button>       
       </Form>
-    </Container>
+    </>
   );
 };
